16160ca649826b42a77639b5225a775568e6834e
AleliLegaspi <aslegaspi@up.edu.ph>
2015-09-04 12:41:59 +0800

    getpostdeleteupdate

--- config/router.js
+++ config/router.js
@@ -3,9 +3,26 @@
 var student = require('./../controllers/student');
 var teacher = require('./../controllers/teacher');
 
-module.exports = function(router){
+module.exports = function(router){//somewhat specifies the link
       router.route('/student')
-            .get(student.find);
+            .get(student.find) //http method get--->> do fxn .find
+            .post(student.insert);
+            
+            
+           
+            
+      router.route('/student/:id')//yung may mga WHERE
+            .get(student.find1)   //.httprequest method
+            .put(student.update)
+            .delete(student.remove);
+      
+            
+            
+            
+            
+            
+            
+            
       
       router.route('/teacher')   
             .get(teacher.get);
